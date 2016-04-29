9a0f714f1310c63a4d5c60ff71c272a015b1a8e7
xljiang <sweetxiaolu@gmail.com>
2016-04-28 21:41:30 -0400

    add palindrome number

---
+++ PalindromeNumber.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+        if ( x < 0 ){
+            return false;
+        }
+        return x == reverse(x);
+    }
+    
+    public int reverse(int x){
+        int res = 0;
+        while (x != 0){
+            res = res * 10 + x % 10;
+            x = x / 10;
+        }
+        return res;
+    }
+}
