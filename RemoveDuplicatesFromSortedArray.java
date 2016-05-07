8adf3891d9aee464f899aab72f9fd9a635a8476c
xljiang <sweetxiaolu@gmail.com>
2016-05-06 21:05:19 -0400

    add remove duplicates from sorted array

---
+++ RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class RemoveDuplicatesFromSortedArray {
+
+	public int removeDuplicates(int[] nums){
+		if (nums == null || nums.length == 0){
+			return 0;
+		}
+		int len = 1;
+		for (int i = 1; i < nums.length; i++){
+			if (nums[i] != nums[i-1]){
+				nums[len] = nums[i];
+				len++;
+			}
+		}
+		return len;
+	}
+}
