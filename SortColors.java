580c2345a19b00b8484488564bfbaf794a1a1723
xljiang <sweetxiaolu@gmail.com>
2016-05-31 20:24:40 -0400

    add sort colors

---
+++ SortColors.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+public class SortColors {
+
+	// method 1: two-pass algorithm, counting sort
+    public void sortColors(int[] nums) {
+        int red = 0;
+        int white = 0;
+        int blue = 0;
+        for(int i = 0; i < nums.length; i++){
+            if (nums[i] == 0) red++;
+            if (nums[i] == 1) white++;
+            if (nums[i] == 2) blue++;
+        }
+        for (int i = 0; i < red; i++){
+            nums[i] = 0;
+        }
+        for (int i = red; i < (red + white); i++){
+            nums[i] = 1;
+        }
+        for (int i = (red + white); i < nums.length; i++){
+            nums[i] = 2;
+        }
+    }
+    
+    // method 2: one-pass algorithm
+    // use two pointer zero and two, and an iterator i; sweep all 0s to the left and all 2s to the right,
+    // then all 1s are left in the middle
+    public void sortColors2(int[] nums){
+    	int zero = 0, two = nums.length - 1;
+    	for (int i = 0; i <= two;){
+    		if (nums[i] == 0){
+    			// swap nums[i] and nums[zero]
+    			int tmp = nums[i];
+    			nums[i] = nums[zero];
+    			nums[zero] = tmp;
+    			i++;
+    			zero++;
+    		} else if (nums[i] == 2){
+    			// swap nums[i] and nums[two]
+    			int tmp = nums[i];
+    			nums[i] = nums[two];
+    			nums[two] = tmp;
+    			two--;
+    		} else {
+    			i++;
+    		}
+    	}
+    }
+}
