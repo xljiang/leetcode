2f1166323c3cfc1f4f5e44ed02a5276502b07b33
xljiang <sweetxiaolu@gmail.com>
2016-07-02 21:34:41 -0400

    add house robber

---
+++ HouseRobber.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class HouseRobber {
+	
+	// a typical weighted independent set problem
+	public int rob(int[] nums){
+        int n = nums.length;
+        if (n == 0) return 0;
+        
+        int[] money = new int[n + 1];
+        money[0] = 0;
+        money[1] = nums[0];
+        for (int i = 2; i < n+1; i++){
+            money[i] = Math.max(money[i-1], money[i-2] + nums[i-1]);
+        }
+        
+        return money[n];
+	}
+}
