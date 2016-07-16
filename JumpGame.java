4855a8632f0f8c37209f41da02ffe5b2d8e0f20f
xljiang <sweetxiaolu@gmail.com>
2016-07-16 00:00:28 -0400

    update jump game

--- JumpGame.java
+++ JumpGame.java
@@ -3,36 +3,19 @@ package leetcode;
 
 public class JumpGame {
 	
-	// Greedy
-	public boolean canJump(int[] nums){
-		if (nums == null || nums.length == 0){
-			return false;
-		}
-		int farthest = nums[0];
-		for (int i = 1; i < nums.length; i++){
-			if (i <= farthest && nums[i] + i >= farthest){
-				farthest = nums[i] + i;
-			}
-		}
-		return farthest >= nums.length - 1;
-	}
-	
-	// Greedy 2
-    public boolean canJump2(int[] nums) {
-        if (nums == null || nums.length == 0){
-			return false;
-		}
-	    int farthest = nums[0];
-	    for (int i = 0; i <= farthest; i++){
-	        farthest = Math.max(farthest, i + nums[i]);
-	        if (farthest >= nums.length - 1){
-	            return true;
-	        }
-	    }
-	    return false;
+	// Greedy: O(n) time, O(1) space
+    public boolean canJump(int[] nums){
+    	if (nums == null || nums.length == 0){
+    		return false;
+    	}
+    	
+    	int farthest = nums[0];
+    	for (int i = 1; i <= farthest && farthest < nums.length; i++){
+    		farthest = Math.max(farthest, i + nums[i]);
+    	}
+    	return farthest >= nums.length - 1;
     }
     
-	// dp
 }
 
 
