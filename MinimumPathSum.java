e4012c87d22c6f23d71a27ff93e996042d2baace
xljiang <sweetxiaolu@gmail.com>
2016-07-27 22:31:14 -0400

    add minimum path sum

---
+++ MinimumPathSum.java
@@ -0,0 +1,53 @@
+package leetcode;
+
+public class MinimumPathSum {
+
+	public int minPathSum(int[][] grid){
+		if (grid == null) return 0;
+		int m = grid.length;
+		int n = grid[0].length;
+		int sum[][] = new int[m][n];
+		
+		// initialize
+		sum[0][0] = grid[0][0];
+		for(int i = 1; i < m; i++){
+			sum[i][0] = sum[i-1][0] + grid[i][0];
+		}
+		for (int j = 1; j < n; j++){
+			sum[0][j] = sum[0][j-1] + grid[0][j];
+		}
+		
+		// dp
+		for (int i = 1; i < m; i++){
+			for (int j = 1; j < n; j++){
+				sum[i][j] = Math.min(sum[i-1][j], sum[i][j-1]) + grid[i][j];
+			}
+		}
+		
+		return sum[m-1][n-1];
+	}
+	
+	// space optimized dp
+	public int minPathSumOpt(int[][] grid){
+		if (grid == null) return 0;
+		int m = grid.length;
+		int n = grid[0].length;
+		int sum[] = new int[n];
+		
+		// initialize
+		sum[0] = 0;
+		for(int j = 1; j < n; j++){
+			sum[j] = Integer.MAX_VALUE;
+		}
+		
+		// dp
+		for (int i = 0; i < m; i++){
+			sum[0] = sum[0] + grid[i][0];
+			for (int j = 1; j < n; j++){
+				sum[j] = Math.min(sum[j-1], sum[j]) + grid[i][j];
+			}
+		}
+		
+		return sum[n-1];
+	}
+}
