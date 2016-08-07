ac4e5934dbe0b2361f6485333e761a3a852a69db
xljiang <sweetxiaolu@gmail.com>
2016-08-07 09:21:40 -0400

    add single number

---
+++ SingleNumber.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+
+public class SingleNumber {
+	
+	public int singleNumber(int[] nums){
+		int result = 0;
+		for (int i = 0; i < nums.length; i++){
+			result ^= nums[i];
+		}
+		return result;
+	}
+}
