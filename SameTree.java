853e766ddbaf8c27466add1b454b1709b41a7cff
xljiang <sweetxiaolu@gmail.com>
2016-10-10 19:59:05 -0400

    update same tree

--- SameTree.java
+++ SameTree.java
@@ -6,7 +6,7 @@ public class SameTree {
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) return true;
-        if (p != null & q != null){
+        if (p != null && q != null){
             return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         } else {
             return false;
