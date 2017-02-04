4a42f03b45ac2021faab507ba3780152d03ed8b3
xljiang <sweetxiaolu@gmail.com>
2017-02-03 22:45:26 -0500

    update climbing stairs

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -3,9 +3,8 @@ package leetcode;
 import java.util.HashMap;
 
 public class ClimbingStairs {
-
+	// memoization + recursion
 	HashMap<Integer, Integer> ways = new HashMap<>();
-	
 	public int climbStairs(int n){
 		if (n <= 0) return 0;
 		if (n == 1 || n == 2) return n;
@@ -19,31 +18,34 @@ public class ClimbingStairs {
 		}
 	}
 	
-	// method 2
-	
-	public int climbStairs2(int n){
-		if (n <= 1) return 1;
-		int last = 1, lastlast = 1;
-		int now = 0;
-		for (int i = 2; i <= n; i++){
-			now = last + lastlast;
-			lastlast = last;
-			last = now;
-		}
-		return now;
-	}
-	
-	
-	// a more general dp solution
+	// dp
 	public int climbStairs3(int n){
-		if (n <= 1) return 1;
-		int ways[] = new int[n+1];
-		ways[0] = 1;
-		ways[1] = 1;
-		ways[2] = 2;
-		for (int i = 2; i <= n; i++){
-			ways[i] = ways[i-1] + ways[i-2];
-		}
-		return ways[n];
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int[] cache = new int[n+1];
+        cache[1] = 1;
+        cache[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            cache[i] = cache[i-1] + cache[i-2];
+        }
+        return cache[n];
 	}
+	
+	// dp with optimized space
+	public int climbStairs2(int n){
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int last = 1; // n = 1
+        int lastlast = 2; // n = 2
+        int now = 0;
+        for (int i = 3; i <= n; i++) {
+            now = last + lastlast;
+            last = lastlast;
+            lastlast = now;
+        }
+        return now;
+	}
+	
+	
+
 }
