8c12f6e0424bafb81877b4b2bdb6a6cab8de89a4
xljiang <sweetxiaolu@gmail.com>
2017-02-06 23:17:17 -0500

    add coin change

---
+++ CoinChange.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, amount+1); // fill array with "amount+1" : an impossible value that larger than amount
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i-coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
