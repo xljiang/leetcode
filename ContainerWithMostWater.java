d0357c4ece617513b8cccb96070f39f8cfa6dcbd
xljiang <sweetxiaolu@gmail.com>
2017-02-11 22:25:44 -0500

    update container with most water

--- ContainerWithMostWater.java
+++ ContainerWithMostWater.java
@@ -15,5 +15,26 @@ public class ContainerWithMostWater {
 		}
 		return area;
 	}
-
+	
+	// a little bit optimization
+    public int maxArea2(int[] height) {
+        int i = 0, j = height.length - 1;
+        int max = 0;
+        while (i < j) {
+            int area = Math.min(height[i], height[j]) * (j - i);
+            max = Math.max(max, area);
+            if (height[i] <= height[j]) { // move i forward
+                int currHeight = height[i];
+                while (i < j && height[i] <= currHeight) {
+                    i++;
+                }
+            } else { // move j backward
+                int currHeight = height[j];
+                while (j > i && height[j] <= currHeight) {
+                    j--;
+                }
+            }
+        }
+        return max;
+    }
 }
