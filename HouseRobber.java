3c58362319667847892acd7e21b65c53fbc3bc30
xljiang <sweetxiaolu@gmail.com>
2017-02-11 22:25:56 -0500

    update house robber

--- HouseRobber.java
+++ HouseRobber.java
@@ -16,4 +16,18 @@ public class HouseRobber {
         
         return money[n];
 	}
+	
+	// space optimized solution
+    public int rob2(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int lastLast = 0;
+        int last = nums[0];
+        int curr = last;
+        for (int i = 2; i <= nums.length; i++) {
+            curr = Math.max(last, lastLast + nums[i-1]);
+            lastLast = last;
+            last = curr;
+        }
+        return curr;
+    }
 }
