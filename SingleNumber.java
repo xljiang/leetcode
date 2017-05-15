2e08d1bc5e84a6db6b9a1ac36b723d907c403e18
xljiang <sweetxiaolu@gmail.com>
2017-05-15 16:08:10 -0400

    updated single number

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,9 +1,30 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class SingleNumber {
-	
-	public int singleNumber(int[] nums){
+	// method 1
+    public int singleNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(set.contains(nums[i])) {
+                set.remove(nums[i]);
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        Iterator<Integer> itr = set.iterator();
+        int res = 0;
+        while(itr.hasNext()) {
+            res = itr.next();
+        }
+        return res;
+    }
+    
+	// method 2, no extra space used
+	public int singleNumber2(int[] nums){
 		int result = 0;
 		for (int i = 0; i < nums.length; i++){
 			result ^= nums[i];
