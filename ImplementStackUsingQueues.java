db2b270eda35bf27739cb2994d646767cf1d9197
xljiang <sweetxiaolu@gmail.com>
2017-05-31 12:57:51 -0400

    cleaner solution with one queue

--- ImplementStackUsingQueues.java
+++ ImplementStackUsingQueues.java
@@ -5,9 +5,49 @@ import java.util.Queue;
 
 public class ImplementStackUsingQueues {
 
-	private Queue<Integer> q = new LinkedList<Integer>(); 
+	// push O(n), pop O(1), top O(1)
+	
+    /** Initialize your data structure here. */
+    Queue<Integer> queue;
+    
+    public ImplementStackUsingQueues() {
+        queue = new LinkedList<>();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        int size = queue.size();
+        queue.add(x);
+        for (int i = 0; i < size; i++) {
+            queue.add(queue.poll());
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return queue.poll();
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        return queue.peek();
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+	
+/* my old implementation
+
+	private Queue<Integer> q;
 
 	// only can use add(), peek(), poll(), size()
+	
+	// instructor
+	public ImplementStackUsingQueues() {
+		q = new LinkedList<Integer>();
+	}
 	
     // Push element x onto stack.
     public void push(int x) {
@@ -40,4 +80,8 @@ public class ImplementStackUsingQueues {
     public boolean empty() {
         return q.isEmpty();
     }
+*/
+	
+
+
 }
