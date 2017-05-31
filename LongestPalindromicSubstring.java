b085ea4b41605c79551630e05c5851c77c29a90d
xljiang <sweetxiaolu@gmail.com>
2017-05-31 13:04:03 -0400

    added brute force and another solution, optimized code

--- LongestPalindromicSubstring.java
+++ LongestPalindromicSubstring.java
@@ -1,40 +1,80 @@
 package leetcode;
 
 public class LongestPalindromicSubstring {
-	// ivars
-    int maxLength, start;
-    
+	// method 1: brute force (TLE), O(n^3) time
     public String longestPalindrome(String s) {
-        if (s == null || s.isEmpty()){
-            return "";
+        int maxLen = 0;
+        int start = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = s.length() - 1; j >= i; j--) {
+                if (isPalindrome(s, i, j)) {
+                    if (j-i+1 > maxLen) {
+                        maxLen = j - i + 1;
+                        start = i;
+                    }
+                }
+            }
         }
-        if (s.length() == 1){
-            return s;
-        }
-        
-        for (int i = 0; i < s.length() - 1; i++){
-            checkPanlindrome(s, i, i);// assume odd length, try to extend Palindrome as possible
-            checkPanlindrome(s, i, i+1);// assume even length
-        }    
-        
-        return s.substring(start, start + maxLength);
+        return s.substring(start, start + maxLen);
     }
     
-    // search from middle to left and right
-    private void checkPanlindrome(String s, int left, int right){
-        while ((left >= 0) && (right < s.length()) && (s.charAt(left) == s.charAt(right))){
+    private boolean isPalindrome(String s, int start, int end) {
+        while (start <= end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    // method 2: extend around center O(n^2) time, O(1) space
+    public String longestPalindrome2(String s) {
+        if (s == null || s.length() == 0) return "";
+        int maxLen = 0;
+        int start = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = extendAroundCenter(s, i, i); // odd length
+            int len2 = extendAroundCenter(s, i, i+1); // even length
+            int len = Math.max(len1, len2);
+            if (len > maxLen) {
+                maxLen = len;
+                start = i - (len-1)/2;
+            }
+        }
+        return s.substring(start, start + maxLen);
+    }
+    
+    // extend as much as possible
+    private int extendAroundCenter(String s, int left, int right) {
+        if (left >= s.length() || right >= s.length()) return 0;
+        while (left >= 0 && right < s.length()) {
+            if (s.charAt(left) != s.charAt(right)) {
+                break;
+            }
             left--;
             right++;
         }
-        
-        // change index to real index in the string
-        left++;
-        right--;
-        
-        // update maxLength and start index
-        if (maxLength < right - left + 1){ 
-            start = left;
-            maxLength = right - left + 1;
-        }
+        return right-left-1;
     }
+    
+    // method 3: O(n^2) time, O(1) space
+    public String longestPalindrome3(String s) {
+        int maxLen = 0;
+        int start = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (isPalindrome(s, i - maxLen - 1, i)) {
+                start = i - maxLen - 1;
+                maxLen += 2;
+            } else if (isPalindrome(s, i - maxLen, i)) {
+                start = i - maxLen;
+                maxLen += 1;
+            }
+        }
+        return s.substring(start, start + maxLen);
+    }
+    
+    /* isPalindrome() method is the same as that in method 1 */
+
 }
