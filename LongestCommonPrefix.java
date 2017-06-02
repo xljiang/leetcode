41490bd746d7cf096445ca6126dce8449b2e743b
xljiang <sweetxiaolu@gmail.com>
2017-06-02 13:37:52 -0400

    updated longest common prefix

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -21,4 +21,22 @@ public class LongestCommonPrefix {
         }
         return scp;
     }
+    
+    // new implementation, easy understand
+    public String longestCommonPrefix2(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        if (strs[0] == null || strs[0].length() == 0) return "";
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return sb.toString();
+                }
+            }
+            sb.append(c);
+        } 
+        return sb.toString();
+    }
 }
