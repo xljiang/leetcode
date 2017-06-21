410ead53c68a5ba9b4dfd796f9d38e7b095ec5b8
xljiang <sweetxiaolu@gmail.com>
2017-06-21 13:04:30 -0400

    add two more solutions

--- SingleNumberII.java
+++ SingleNumberII.java
@@ -30,6 +30,22 @@ public class SingleNumberII {
     	}
     	return result;
     }
-	
+    
+    // method 2 improvement 1: one pass
+    public int singleNumber3(int[] nums) {
+        int result=0;
+        int[] bits=new int[32];
+        for (int i = 0; i < 32; i++) {
+            for(int j = 0; j < nums.length; j++) {
+                bits[i] += ((nums[j] >> i) & 1);
+                bits[i] %= 3;
+            }
+            result |= (bits[i] << i);
+        }
+        return result;
+    }
+
+	// method 3 improvement 2: one pass + no extra bits array
+    
 	
 }
